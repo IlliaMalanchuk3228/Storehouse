48b929ce586e8d68007f78a252668ac676f996d7
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-20 19:01:15 +0300

    Updated

--- DAL/Repositories/Interfaces/IOrderRepository.cs
+++ DAL/Repositories/Interfaces/IOrderRepository.cs
@@ -1,12 +1,13 @@
 using System.Threading.Tasks;
 using DAL.DataModels;
 using DAL.Entities;
-
+ 
 namespace DAL.Repositories
 {
     public interface IOrderRepository : IRepository<OrderModel>
     {
         Task Create(OrderModel orderModel);
+        Task Update(OrderModel orderModel);
         Task<OrderModel> GetOrderByIdAsync(int id);
         
     }
