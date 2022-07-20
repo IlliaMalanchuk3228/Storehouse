8cf05a989b1961328060c2fe8930da7ccf29e915
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-20 19:01:47 +0300

    Updated

--- DAL/Repositories/Interfaces/IRepository.cs
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -8,7 +8,6 @@ namespace DAL.Repositories
     public interface IRepository<TEntity> where TEntity : class, new()
     {
         IEnumerable<TEntity> GetAll();
-        // Task<TEntity> GetById(int id);
         Task<TEntity> AddAsync(TEntity entity);
         Task<TEntity> UpdateAsync(TEntity entity);
     }
