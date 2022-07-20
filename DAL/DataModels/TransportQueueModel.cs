164a2c38b8e95fbad93127e8ff269097fd5f6ace
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-20 18:57:04 +0300

    Updated

--- DAL/DataModels/TransportQueueModel.cs
+++ DAL/DataModels/TransportQueueModel.cs
@@ -1,14 +1,12 @@
 using System.Collections.Generic;
 
 namespace DAL.DataModels
-{
+{ 
     public class TransportQueueModel
     {
         public virtual int Id { get; set; }
         public virtual bool IsShipping { get; set; }
         public virtual int BuyQueueId { get; set; }
         public virtual int ProductId { get; set; }
-        public virtual IEnumerable<ProductModel> ProductModels { get; set; }
-        public virtual IEnumerable<BuyQueueModel> BuyQueueModels { get; set; }
     }
 }
