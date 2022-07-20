54c9ff74b1ce1e6c90bab3be1a0704fe67111102
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-20 18:56:32 +0300

    Updated

--- DAL/DataModels/OrderModel.cs
+++ DAL/DataModels/OrderModel.cs
@@ -4,11 +4,12 @@ using DAL.Entities;
 namespace DAL.DataModels
 {
     public class OrderModel
-    {
+    { 
         public virtual int Id { get; set; }
         public virtual bool IsSold { get; set; }
         public virtual bool IsDelivered { get; set; }
-        public virtual IEnumerable<ProductModel> Products { get; set; }
-        public virtual CustomerModel Customers { get; set; }
+        public virtual int ProductId { get; set; }
+        public virtual int CustomerId { get; set; }
+        
     }
 }
