0487dd55c76794d543e4ab5c531a6315467db661
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-20 19:00:35 +0300

    Updated

--- DAL/Repositories/Interfaces/IBuyQueueRepository.cs
+++ DAL/Repositories/Interfaces/IBuyQueueRepository.cs
@@ -3,10 +3,11 @@ using DAL.DataModels;
 using DAL.Entities;
 
 namespace DAL.Repositories
-{
+{ 
     public interface IBuyQueueRepository : IRepository<BuyQueueModel>
     {
         Task Create(BuyQueueModel buyQueueModel);
+        Task Update(BuyQueueModel buyQueueModel);
         Task<BuyQueueModel> GetBuyQueueByIdAsync(int id);
     }
 }
