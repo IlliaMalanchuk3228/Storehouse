a0275af4ee4ebffb10789e23ee58ab2388cfe833
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-20 18:55:45 +0300

    Updated

--- DAL/DataModels/BuyQueueModel.cs
+++ DAL/DataModels/BuyQueueModel.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 namespace DAL.DataModels
 {
     public class BuyQueueModel
-    {
+    { 
         public virtual int Id { get; set; }
         public virtual bool IsBuying { get; set; }
         public virtual int ProductId { get; set; }
-        public virtual IEnumerable<ProductModel> ProductModels { get; set; }
     }
 }
