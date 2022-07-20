546b5b86043ea97c0b4aea14fbab74b8dc5f6a97
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-20 18:51:10 +0300

    Updated

--- BLL/Iml/CustomerManager.cs
+++ BLL/Iml/CustomerManager.cs
@@ -10,8 +10,7 @@ namespace BLL.Iml
     public class CustomerManager : ICustomerManager
     {
         private readonly IUnitOfWork _unitOfWork;
-
-        
+         
         public CustomerManager(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -26,30 +25,6 @@ namespace BLL.Iml
         {
             await _unitOfWork.CustomerRepository.Create(customerModel);
         }
-
-        // public async Task AddOrderToCustomer(int idCustomer, int orderId, Order orderr)
-        // {
-        //     var orders = await _unitOfWork.OrderRepository.GetOrderByIdAsync(orderId);
-        //     var customer = await _unitOfWork.CustomerRepository.GetCustomerByIdAsync(idCustomer);
-        //
-        //     orderr = orders;
-        //     
-        //     customer.Orders = new List<Order>()
-        //     {
-        //         orderr
-        //     };
-        //     await _unitOfWork.CustomerRepository.UpdateAsync(customer);
-        // }
-        // public async Task JoinOrderAndCustomer(int orderId, int customerId)
-        // {
-        //     var orders = await _unitOfWork.OrderRepository.GetOrderByIdAsync(orderId);
-        //     var customer = await _unitOfWork.CustomerRepository.GetCustomerByIdAsync(customerId);
-        //
-        //     customer.Orders = new List<Order>()
-        //     {
-        //         orders
-        //     };
-        //     await _unitOfWork.CustomerRepository.UpdateAsync(customer);
-        // }
+        
     }
 }
