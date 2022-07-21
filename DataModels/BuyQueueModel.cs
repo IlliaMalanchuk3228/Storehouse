bd02364145acd8587ec530c20e29adad2ec1154b
I.Malanchuk <56121409+IlliaMalanchuk3228@users.noreply.github.com>
2022-07-21 20:30:21 +0300

    Delete DataModels directory

--- DataModels/BuyQueueModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DataModels
-{
-    public class BuyQueueModel
-    {
-        public virtual int BuyQueueId { get; set; }
-        public virtual bool IsBuying { get; set; }
-        public virtual int ProductId { get; set; }
-    }
-}
